fb54b49a3a71cac6655fe52f4c509eea780cfb87
onurravli <onurravli@hotmail.com>
2024-01-19 18:41:25 +0300

    chore(cli): init

---
+++ cli/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log(
+  "Hi, I'm Onur. I'm a 22-year-old full-stack developer living in Turkey, currently in the 3rd year of computer engineering at Konya Technical University."
+);
